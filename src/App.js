d3b21dd0e7c90648d6ad1952d4aac620e5f6b993
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-14 15:43:08 +0200

    Use Effect to Fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://api.github.com/users/lucian-aionicesei 
+
+function App({login}) {
+const [data, setData] = useState(null);
+
+useEffect (() => {
+  fetch(`https://api.github.com/users/${login}`)
+  .then (response => response.json())
+  .then(setData);
+}, [])
+
+if (data) {
+  return <div>{JSON.stringify(data)}</div>;
+}
+
+return <div>No user available</div>
 }
 
 export default App;
