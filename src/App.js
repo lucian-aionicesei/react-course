6dac831014e62bdb5440b745275acd119d4e5fa4
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-05-14 17:33:11 +0200

    React Router and the Link component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Events, Contact, Whoops404 } from "./pages";
 
-// https://api.github.com/users/lucian-aionicesei 
+// https://api.github.com/users/lucian-aionicesei
 
-function App({login}) {
-const [data, setData] = useState(null);
-const [loading, setLoading] = useState(false);
-const [error, setError] = useState(null);
-
-useEffect (() => {
-  if(!login) return;
-  setLoading(true);
-  fetch(`https://api.github.com/users/${login}`)
-  .then (response => response.json())
-  .then(setData)
-  .then(() => setLoading(false))
-  .catch(setError);
-}, [login])
-
-if (loading) return <h1>Loading...</h1>;
-if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
-if (!data) return null;
-
-if (data) {
-  return <div>
-    <h1>{data.login}</h1>
-    <p>{data.id}</p>
-    <img src={data.avatar_url} alt={data.login} />
-    </div>;
-}
-
-return <div>No user available</div>
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="events" element={<Events />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
